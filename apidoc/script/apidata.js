30cda8f35c782c68e0829f5e92e1e06b1ae58c89
sunyy9 <sunyy9@lenovo.com>
2018-11-02 13:30:03 +0800

    update url

--- apidoc/script/apidata.js
+++ apidoc/script/apidata.js
@@ -1,35 +1,36 @@
 ﻿var apidata=[
     {   owner:"孙宜营", 
-        url:"http://dianpinguat.lenovo.com.cn:8088//index.html",
+        url:"http://csapitest.lenovo/authorization/apidoc/index.html",
         name:"用户权限",
         display:true  
     },
     {   owner:"屠海涛", 
-        url:"http://10.99.101.139:8082/index.html",
+        url:"http://csapitest.lenovo/serviceorder/apidoc/index.html",
         name:"单据中心",
         display:true  
     },
     {   owner:"屠海涛", 
-        url:"http://10.99.101.139:8084/index.html",
+        url:"http://csapitest.lenovo/serviceevent/apidoc/index.html",
         name:"事件中心",
         display:true  
     },
     { 
         owner:"屠海涛", 
-        url:"http://10.99.101.139:8086/index.html",
+        url:"http://csapitest.lenovo/serviceattach/apidoc/index.html",
         name:"附件中心",
         display:true  
     },
     { 
         owner:"隋广武", 
-        url:"http://lcseuat.lenovo.com.cn:89/index.html",
+        url:"http://csapitest.lenovo/customercenter/apidoc/index.html",
         name:"用户中心",
         display:true  
     },
     {
         owner:"张潇逸",
-        url:"http://10.99.101.68:8096/local/index.html",
+        url:"http://csapitest.lenovo/productcenter/apidoc/index.html",
         name:"产品中心",
         display:true
+
     }
 ];
