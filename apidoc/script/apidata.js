ad9dbbbc9ca1f9949076bb0442f6b13271e85af3
sunyy9 <sunyy9@lenovo.com>
2019-02-02 09:30:34 +0800

    增加服务鉴权接口文档

--- apidoc/script/apidata.js
+++ apidoc/script/apidata.js
@@ -1,36 +1,46 @@
 ﻿var apidata=[
     {   owner:"孙宜营", 
-        url:"http://csapitest.lenovo/authorization/apidoc/index.html",
-        name:"用户权限",
-        display:true  
-    },
-    {   owner:"屠海涛", 
-        url:"http://csapitest.lenovo/serviceorder/apidoc/index.html",
-        name:"单据中心",
-        display:true  
-    },
-    {   owner:"屠海涛", 
-        url:"http://csapitest.lenovo/serviceevent/apidoc/index.html",
-        name:"事件中心",
-        display:true  
-    },
-    { 
-        owner:"屠海涛", 
-        url:"http://csapitest.lenovo/serviceattach/apidoc/index.html",
-        name:"附件中心",
-        display:true  
-    },
-    { 
-        owner:"隋广武", 
-        url:"http://csapitest.lenovo/customercenter/apidoc/index.html",
-        name:"用户中心",
-        display:true  
-    },
-    {
-        owner:"张潇逸",
-        url:"http://csapitest.lenovo/productcenter/apidoc/index.html",
-        name:"产品中心",
-        display:true
-
-    }
-];
+            url:"http://dianpinguat.lenovo.com.cn:8087/gateway/doc/index.html",
+            name:"服务鉴权",
+            display:true  
+        },    
+    {   owner:"孙宜营", 
+            url:"http://csapitest.lenovo/authorization/apidoc/index.html",
+            name:"用户权限",
+            display:true  
+        },
+        {   owner:"屠海涛", 
+            url:"http://csapitest.lenovo/serviceorder/apidoc/index.html",
+            name:"单据中心",
+            display:true  
+        },
+        {   owner:"屠海涛", 
+            url:"http://csapitest.lenovo/serviceevent/apidoc/index.html",
+            name:"事件中心",
+            display:true  
+        },
+        { 
+            owner:"屠海涛", 
+            url:"http://csapitest.lenovo/serviceattach/apidoc/index.html",
+            name:"附件中心",
+            display:true  
+        },
+        { 
+            owner:"隋广武", 
+            url:"http://csapitest.lenovo/customercenter/apidoc/index.html",
+            name:"用户中心",
+            display:true  
+        },
+        {
+            owner:"张潇逸",
+            url:"http://csapitest.lenovo/productcenter/apidoc/apidoc/index.html",
+            name:"产品中心",
+            display:true
+        },
+        {
+        owner:"孙宜营",
+            url:"http://csapitest.lenovo/dpcenter/apidoc/index.html",
+            name:"点评中心",
+            display:true
+        }
+    ];
